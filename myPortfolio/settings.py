de709f5ebed67586aab02008ac9dc8cda92279b3
Favour Adebose <favouradebose@gmail.com>
2024-08-18 20:13:24 +0100

    updated settings

--- myPortfolio/settings.py
+++ myPortfolio/settings.py
@@ -126,6 +126,8 @@ STATICFILES_DIR = [
     STATIC_DIR
 ]
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
